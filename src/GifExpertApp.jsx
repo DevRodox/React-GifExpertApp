eba37f73c1ecdcc326a69426793fee729bb24d86
Rodox <rodrigo17.ed@gmail.com>
2023-12-20 17:25:11 -0600

    Global styles were added

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,7 +2,13 @@
 export const GifExpertApp = () => {
     return (
         <>
+            {/* Titulo */}
             <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            {/* Listado de items/gifs */}
+                {/* Gif item */}
         </>
     );
 };
