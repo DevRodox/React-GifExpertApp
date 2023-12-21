0826205d9b7baff711f9d25146f6aab5f61ada39
Rodox <rodrigo17.ed@gmail.com>
2023-12-20 22:42:41 -0600

    AddCategory Component were implemented

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([ 'One Punch' ]);
+
+    const onAddCategory = ( newCategory ) => {
+
+        if( categories.includes(newCategory) ) return;
+
+        setCategories( [ newCategory, ...categories] );
+        // setCategories( cat => [...cat, 'valorant']);
+    };
+
     return (
         <>
             {/* Titulo */}
             <h1>GifExpertApp</h1>
 
             {/* Input */}
+            <AddCategory 
+                // setCategories = { setCategories }
+                onNewCategory = { onAddCategory } 
+            />
 
             {/* Listado de items/gifs */}
+
+            <ol>
+                {
+                    categories.map( cat => {
+                        return <li key={ cat }> { cat } </li>
+                    })
+                }
+            </ol>
+
                 {/* Gif item */}
         </>
     );
