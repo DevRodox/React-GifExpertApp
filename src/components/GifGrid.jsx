c33cc393bea9b7c90356d344441fb151094b95ae
Rodox <rodrigo17.ed@gmail.com>
2023-12-22 14:10:52 -0600

    GifItem were added

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,21 +1,35 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { GifItem } from './GifItem';
 import { getGifs } from '../helpers/getGifs';
-
 
 export const GifGrid = ({ category }) => {
 
-    const [counter, setCounter] = useState(10);
+    const [images, setImages] = useState([]);
+
+    const getImages = async()=> {
+        const newImages = await getGifs( category );
+        setImages( newImages );
+    };
+
 
     useEffect( () => {
-        getGifs( category ); 
-    }, [ ]);
+        getImages();
+    }, []);
     
     return (
         <>
             <h3>{ category }</h3>
 
-            <h5>{ counter }</h5>
-            <button onClick={ () => setCounter( counter + 1 ) }> +1 </button>
+            <div className='card-grid' >
+                {
+                    images.map( ( image ) => (
+                        <GifItem 
+                            key={ image.id }
+                            { ...image }
+                        />
+                    ))
+                }
+            </div>
         </>
     );
 };
