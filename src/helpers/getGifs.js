7527fd1edda3c7228587efef2ef2b043e0258c58
Rodox <rodrigo17.ed@gmail.com>
2023-12-21 18:16:40 -0600

    useEffect hook were implemented

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 export const getGifs = async( category ) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=C8bWXwqODnCl56QIcl8N5dTc5FWKovIK&q=${ category }&limit=20`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=C8bWXwqODnCl56QIcl8N5dTc5FWKovIK&q=${ category }&limit=10`
     const resp = await fetch( url );
     const { data } = await resp.json();
 
