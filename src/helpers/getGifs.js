bd5b706c97d1c134c3fb816104d2c17c87e71232
Rodox <rodrigo17.ed@gmail.com>
2023-12-26 14:09:33 -0600

    getGifs tests were written

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,14 +1,18 @@
 
 export const getGifs = async( category ) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=C8bWXwqODnCl56QIcl8N5dTc5FWKovIK&q=${ category }&limit=10`
-    const resp = await fetch( url );
-    const { data } = await resp.json();
+    try{
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=C8bWXwqODnCl56QIcl8N5dTc5FWKovIK&q=${ category }&limit=10`
+        const resp = await fetch( url );
+        const { data } = await resp.json();
 
-    const gifs = data.map( img => ({
-        id: img.id,
-        title: img.title,
-        url: img.images.downsized_medium.url
-    }));
+        const gifs = data.map( img => ({
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }));
 
-    return gifs;
+        return gifs;
+    }catch(error){
+        console.error('Error in getGifs:', error);
+    }
 };
